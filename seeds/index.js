981610eea568eea641870692c0d523d5dd53e816
Eshan V Gopal <32702631+evgopal@users.noreply.github.com>
2021-02-10 23:03:34 +0530

    Add files via upload

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,39 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {descriptors, places } = require('./seedNamer');
 
+mongoose.connect('mongodb://localhost:27017/camp', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Connection Open!!!");
+    })
+    .catch(err => {
+        console.log("Oh No error!!");
+        console.log(err);
+    })
+
+
+const sample = array => array[Math.floor(Math.random()*array.length)]
+
+const seedDb = async () => {
+    await Campground.deleteMany({});
+    for (let i=0; i<50; i++)
+    {
+        const rand = Math.floor(Math.random()*1000);
+        const camp = new Campground({
+            location : `${cities[rand].city}, ${cities[rand].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+
+}
+
+seedDb().then(() => {
+    mongoose.connection.close();
+
+});
