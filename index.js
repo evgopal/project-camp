c7de010a3e15ae6caa630815b2e3274940b71121
Eshan V Gopal <32702631+evgopal@users.noreply.github.com>
2021-02-11 19:20:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
@@ -20,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/camp', {
     })
 
 
-
+app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
